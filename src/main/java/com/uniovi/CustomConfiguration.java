d3116d5fd17e17d8c470acf08f6b9f0a00d962b5
marcialfps <marcialfco@live.com>
2019-07-03 11:02:27 +0100

    Dynamic files is now working.

--- src/main/java/com/uniovi/CustomConfiguration.java
+++ src/main/java/com/uniovi/CustomConfiguration.java
@@ -5,9 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class CustomConfiguration {
+public class CustomConfiguration extends WebMvcConfigurerAdapter {
 
 	@Bean(name = "multipartResolver")
 	public CommonsMultipartResolver multipartResolver() {
@@ -20,4 +22,14 @@ public class CustomConfiguration {
 	public PasswordEncoder passwordEncoder() {
 	    return new BCryptPasswordEncoder();
 	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		String projectPath = System.getProperty("user.dir");
+		String imagesPath = "file:///"+projectPath+"/src/main/resources/static/images/";
+		registry.addResourceHandler("/images/**").addResourceLocations(imagesPath);
+		
+		String videosPath = "file:///"+projectPath+"/src/main/resources/static/videos/";
+		registry.addResourceHandler("/videos/**").addResourceLocations(videosPath);
+	 }
 }
